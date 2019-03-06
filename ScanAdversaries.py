9b748656ed7079bc52f3784b6b6dfe9f1b71c553
Bryan Ostdiek <bostdiek@uoregon.edu>
2019-03-06 14:49:54 -0800

    Load data, split, and scale

--- ScanAdversaries.py
+++ ScanAdversaries.py
@@ -36,3 +36,80 @@ plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
 plt.rcParams['xtick.top'] = True
 plt.rcParams['ytick.right'] = True
+
+
+# ********** Load data *******************************
+SignalDF = pd.read_csv('data/BSM_WW-jet_RelaxedMassCut/data_sig.txt',
+                       skiprows=2,
+                       index_col=False,
+                       names=['(pruned)m', 'pT', 'tau_(1)^(1/2)',
+                              'tau_(1)^(1)', 'tau_(1)^(2)',
+                              'tau_(2)^(1/2)', 'tau_(2)^(1)', 'tau_(2)^(2)',
+                              'tau_(3)^(1/2)', 'tau_(3)^(1)', 'tau_(3)^(2)',
+                              'tau_(4)^(1)', 'tau_(4)^(2)']
+                       )
+BackgroundDF = pd.read_csv('data/BSM_WW-jet_RelaxedMassCut/data_bkg.txt',
+                           skiprows=2,
+                           index_col=False,
+                           names=['(pruned)m', 'pT', 'tau_(1)^(1/2)',
+                                  'tau_(1)^(1)', 'tau_(1)^(2)',
+                                  'tau_(2)^(1/2)', 'tau_(2)^(1)', 'tau_(2)^(2)',
+                                  'tau_(3)^(1/2)', 'tau_(3)^(1)', 'tau_(3)^(2)',
+                                  'tau_(4)^(1)', 'tau_(4)^(2)']
+                           )
+
+columns = SignalDF.columns
+labels = [r'$m_j$ [GeV]', r'$p_T$ [GeV]',
+          r'$\tau_1^{1/2}$', r'$\tau_1^{1}$',
+          r'$\tau_2^{1/2}$', r'$\tau_2^{1}$', r'$\tau_2^{2}$',
+          r'$\tau_3^{1/2}$', r'$\tau_3^{1}$', r'$\tau_3^{2}$',
+          r'$\tau_4^{1}$', r'$\tau_4^{2}$'
+         ]
+
+TrainingColumns = columns
+
+SignalDF['Label'] = 1
+BackgroundDF['Label'] = 0
+
+CombinedData = np.vstack([SignalDF[TrainingColumns].values,
+                          BackgroundDF[TrainingColumns].values
+                          ]
+                         )
+CombinedLabels = np.hstack([SignalDF['Label'].values,
+                            BackgroundDF['Label'].values
+                            ]
+                           ).reshape(CombinedData.shape[0], 1)
+
+if not os.path.isfile('data/TrainingIndices.npy'):
+    indices = np.arange(CombinedLabels.shape[0])
+    np.random.shuffle(indices)
+    training_size = int(0.7 * len(indices))
+    validation_size = int(0.15 * len(indices))
+    TrainingIndices = indices[: training_size]
+    ValIndices = indices[training_size: training_size + validation_size]
+    TestIndices = indices[training_size + validation_size:]
+
+    np.save('data/TrainingIndices.npy', TrainingIndices)
+    np.save('data/ValidationIndices.npy', ValidationIndices)
+    np.save('data/TestIndices.npy', TestIndices)
+else:
+    TrainingIndices = np.load('data/TrainingIndices.npy')
+    ValIndices = np.load('data/ValidationIndices.npy')
+    TestIndices = np.load('data/TestIndices.npy')
+
+X_train, y_train = CombinedData[TrainingIndices], CombinedLabels[TrainingIndices]
+X_test, y_test = CombinedData[TestIndices], CombinedLabels[TestIndices]
+X_val, y_val = CombinedData[ValIndices], CombinedLabels[ValIndices]
+
+mass_train = X_train[:, 0]
+mass_test = X_test[:, 0]
+mass_val = X_val[:, 0]
+
+class_weights = {1: float(len(CombinedData)) / len(SignalDF),
+                 0: float(len(CombinedData)) / len(BackgroundDF)
+                 }
+
+SS = StandardScaler()
+X_trainscaled = SS.fit_transform(X_train)
+X_testscaled = SS.transform(X_test)
+X_valscaled = SS.transform(X_val)
